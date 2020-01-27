bfa3064dbade0e428ebb1d591e76e7ab857b0cd5
sujeongHeo <gjtnwjd40@gmail.com>
2020-01-27 10:26:20 +0900

    fetch 와  state , stateless 에 대해서 배움

--- src/Movie.js
+++ src/Movie.js
@@ -2,36 +2,26 @@ import React ,{Component} from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component{
-
-    static propTypes ={
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-    render(){
-        console.log(this.props);
-        return(
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        );
-    }
+function Movie({title, poster}){
+    return(
+        <div>
+            <MoviePoster poster={poster} />
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
-class MoviePoster extends Component{
-
-    static propTypes={
-        poster: PropTypes.string.isRequired
-    }
-
-    render(){
-        console.log(this.props)
-        return(
-            // <h1>hi</h1>
-            <img src={this.props.poster}/>
-            )
-    }
+function MoviePoster({poster}){
+    return (
+        <img src={poster} alt="Movie Poster" />
+    )
+}
+Movie.propTypes ={
+    title: PropTypes.string.isRequired,
+    poster:  PropTypes.string.isRequired
 }
 
+MoviePoster.propTypes = {
+    poster : PropTypes.string.isRequired
+}
 export default Movie;
