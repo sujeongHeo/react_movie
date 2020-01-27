7c02cf866ab61c485f708b00ff2febde3ed91c4c
sujeongHeo <gjtnwjd40@gmail.com>
2020-01-27 14:30:34 +0900

    Updates

---
+++ precache-manifest.83eca860ef44332e8909edea377b6252.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2f7779864510cf3ad6434f47e3b4417",
+    "url": "/react-movie/index.html"
+  },
+  {
+    "revision": "1d5e8c1632d0885c0162",
+    "url": "/react-movie/static/css/main.37164e2b.chunk.css"
+  },
+  {
+    "revision": "f51e54a58878b4191cab",
+    "url": "/react-movie/static/js/2.272e4bc1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-movie/static/js/2.272e4bc1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1d5e8c1632d0885c0162",
+    "url": "/react-movie/static/js/main.723d3126.chunk.js"
+  },
+  {
+    "revision": "b433bb582968917ab29f",
+    "url": "/react-movie/static/js/runtime-main.a248410b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-movie/static/media/logo.5d5d9eef.svg"
+  }
+]);
